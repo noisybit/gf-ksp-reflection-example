0940e725cf251c8299ecc383bf86470664dd4e68
Blaine Boman <blaine.boman@gmail.com>
2016-06-16 02:03:11 -0400

    initial commit

---
+++ source/GFInterface.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace GameframerReflection
+{
+    public class GFInterface : MonoBehaviour
+    {
+        AssemblyLoader.LoadedAssembly GF;
+        Type PublicInterfaceType;
+        MethodInfo registerEventMethod;
+        MethodInfo deregisterEventMethod;
+        MethodInfo captureEventMethod;
+        object PublicInterfaceInstance;
+        string lastStatus;
+
+        public void Update()
+        {
+            if (GF == null)
+            {
+                GF = AssemblyLoader.loadedAssemblies.SingleOrDefault(a => a.dllName == "Gameframer");
+                PublicInterfaceType = GF.assembly.GetTypes().FirstOrDefault(t => t.Name == "GFPublicInterface");
+                registerEventMethod = PublicInterfaceType.GetMethod("RegisterEvent");
+                deregisterEventMethod = PublicInterfaceType.GetMethod("DeregisterEvent");
+                captureEventMethod = PublicInterfaceType.GetMethod("CaptureEvent");
+                PublicInterfaceInstance = MonoBehaviour.FindObjectsOfType(PublicInterfaceType).FirstOrDefault();
+            }
+        }
+
+        public string GetLastStatus()
+        {
+            return lastStatus;
+        }
+         
+        public void RegisterEvent(string id, string eventName)
+        {
+            if (PublicInterfaceInstance != null)
+            {
+                object[] parametersArray = new object[] { id, eventName };
+                lastStatus = DateTime.Now.ToFileTimeUtc() + " : " + (bool)registerEventMethod.Invoke(PublicInterfaceInstance, parametersArray);
+            }
+            else
+            {
+                UnityEngine.Debug.Log("RegisterEvent: PublicInterfaceInstance is null :(");
+            }
+        }
+        public void DeregisterEvent(string id, string eventName)
+        {
+            if (PublicInterfaceInstance != null)
+            {
+                object[] parametersArray = new object[] { id, eventName };
+                lastStatus = DateTime.Now.ToFileTimeUtc() + " : " + (bool)deregisterEventMethod.Invoke(PublicInterfaceInstance, parametersArray);
+            }
+            else
+            {
+                UnityEngine.Debug.Log("DeregisterEvent: PublicInterfaceInstance is null :(");
+            }
+        }
+
+        public void CaptureEvent(string id, string name, string description)
+        {
+            if (PublicInterfaceInstance != null)
+            {
+                object[] parametersArray = new object[] { id, name, description };
+                lastStatus = DateTime.Now.ToFileTimeUtc() + " : " + (bool)captureEventMethod.Invoke(PublicInterfaceInstance, parametersArray);
+            }
+            else
+            {
+                UnityEngine.Debug.Log("CaptureEvent: PublicInterfaceInstance is null :(");
+            }
+        }
+    }
+}
---
+++ source/MainWindow.cs
@@ -0,0 +1,58 @@
+﻿using KSPPluginFramework;
+using UnityEngine;
+
+namespace GameframerReflection
+{
+    /* KAMR : Kerbal Automated Mission Recorder */
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    [WindowInitials(Caption = "Gameframer Reflection", Visible = true, DragEnabled = true, ClampToScreen = true, TooltipsEnabled = true)]
+    public class MainWindow : MonoBehaviourWindow
+    {
+        public static int MAIN_WIDTH = 230;
+        public static int MAIN_HEIGHT = 100;
+        private static string ID = "GF_TEST";
+        private static string EVENT_NAME = "onFoobar";
+        GFInterface si;
+
+        #region Overrides
+        internal override void Awake()
+        {
+            WindowRect = new Rect(200, 200, MAIN_WIDTH, MAIN_HEIGHT);
+            Visible = true;
+            UnityEngine.Debug.Log("Tester awake, si = " + si);
+            si = gameObject.AddComponent<GFInterface>();
+        }
+
+        internal override void OnDestroy()
+        {
+        }
+
+        internal override void DrawWindow(int id)
+        {
+            GUILayout.BeginVertical();
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Register"))
+            {
+                si.RegisterEvent(ID, EVENT_NAME);
+            }
+            if (GUILayout.Button("Deregister"))
+            {
+                si.DeregisterEvent(ID, EVENT_NAME);
+            }
+            GUILayout.EndHorizontal();
+            if (GUILayout.Button("Do it!"))
+            {
+                si.CaptureEvent(ID, EVENT_NAME, "this is a description");
+            }
+            /*if (GUILayout.Button("Register Random"))
+            {
+                si.RegisterEvent(ID, EVENT_NAME + Random.Range(0, 10000));
+            }*/
+            GUILayout.Label("Last: " + si.GetLastStatus());
+
+            GUILayout.EndVertical();
+        }
+        #endregion
+    }
+}
